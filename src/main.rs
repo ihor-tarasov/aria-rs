a4a9e29f119b10c605a98db5b91f94457dccc723
ihor-tarasov <mov.ax.38@gmail.com>
2022-04-21 19:50:38 +0300

    Basic VM

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+// 2 + 2 * 2
+
+mod opcodes {
+    pub const END: u8 = 0x00;
+    pub const CONST: u8 = 0x01;
+    pub const ADD: u8 = 0x02;
+    pub const MUL: u8 = 0x03;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let constants = [
+        2
+    ];
+
+    let program = [
+        opcodes::CONST, 0,
+        opcodes::CONST, 0,
+        opcodes::MUL,
+        opcodes::CONST, 0,
+        opcodes::ADD,
+        opcodes::END,
+    ];
+
+    let mut stack: Vec<i32> = Vec::new();
+
+    let mut pc = program.iter().cloned();
+    while let Some(opcode) = pc.next() {
+        match opcode {
+            opcodes::END => break,
+            opcodes::CONST => stack.push(*constants.get(pc.next().unwrap() as usize).unwrap()),
+            opcodes::ADD => {
+                let rhs = stack.pop().unwrap();
+                let lhs = stack.pop().unwrap();
+                stack.push(lhs.wrapping_add(rhs));
+            },
+            opcodes::MUL => {
+                let rhs = stack.pop().unwrap();
+                let lhs = stack.pop().unwrap();
+                stack.push(lhs.wrapping_mul(rhs));
+            },
+            _ => panic!(),
+        }
+    }
+
+    if let Some(value) = stack.pop() {
+        println!("{}", value);
+    }
 }
